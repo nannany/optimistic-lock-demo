6358dae196ef094f543fc926daa2817448ef972d
nannany <ymym1990ymym@gmail.com>
2019-09-14 16:32:58 +0900

    rename

---
+++ src/main/java/nannany/optimistic/demo/Result.java
@@ -0,0 +1,11 @@
+package nannany.optimistic.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result{
+}
